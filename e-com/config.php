2e72d1ebe9407221d64922bd1de0c1af3bd755f0
Lakshit Bagul <79861803+lakshitbagul777@users.noreply.github.com>
2024-10-16 22:42:20 +0530

    Update config.php

--- e-com/config.php
+++ e-com/config.php
@@ -4,8 +4,8 @@ define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'e-com');
-define('API_KEY_ID','rzp_test_7VF0CWufBdLEVg');
-define('API_KEY_SECRET','PgpaFEp5WtPehbEonbIDweqU');
+define('API_KEY_ID','');
+define('API_KEY_SECRET','');
 
 define('UPLOAD_DIR', 'uploads/'); 
 
